4d263221bb2526a3a19fcca539e882c8da92c4ae
pauli <pjseoane@gmail.com>
2026-03-10 21:03:45 -0300

    1st comit

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import streamlit as st
 import pandas as pd
-#from datetime import timedelta,datetime
-#import plotly.graph_objects as go
+from datetime import timedelta,datetime
+import plotly.graph_objects as go
 from pjs_qlab.data.YahooPriceFetcher import YahooPriceFetcher as price_fetcher
 from pjs_qlab.analytics.cQuantClass import cQuantClass as cQuant
 
